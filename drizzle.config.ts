829f7148f3ad290db69c424d591c5adb521d0377
Phoeun-Chhantana <42382202+Phoeun-Chhantana@users.noreply.github.com>
2025-08-11 13:32:56 +0700

    update drizzle.config.ts

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,13 +1,13 @@
 import { defineConfig } from "drizzle-kit";
-import fs from 'fs';
-import path from 'path';
+// import fs from 'fs';
+// import path from 'path';
 
 if (!process.env.DATABASE_URL) {
   throw new Error("DATABASE_URL, ensure the database is provisioned");
 }
 
 // const certPath = path.resolve(import.meta.dirname, '../certs/ca-certificate.crt')
-const certPath = process.env.NODE_EXTRA_CA_CERTS ?? ""
+//const certPath = process.env.DATABASE_CA_CERT ?? ""
 
 export default defineConfig({
   out: "./migrations",
@@ -16,8 +16,9 @@ export default defineConfig({
   dbCredentials: {
     url: process.env.DATABASE_URL,
     ssl: process.env.NODE_ENV === "production" ? {
-      rejectUnauthorized: false,
-      ca: fs.readFileSync(certPath).toString()
+      rejectUnauthorized: true,
+      //ca: fs.readFileSync(certPath).toString()
+      ca: process.env.DATABASE_CA_CERT
     } : "require"
   },
 });
