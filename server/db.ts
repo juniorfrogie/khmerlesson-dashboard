3bac8ad0e93282dd90bca14ffe6384f4869bf078
Phoeun-Chhantana <42382202+Phoeun-Chhantana@users.noreply.github.com>
2025-08-11 16:59:50 +0700

    update db.ts library

--- server/db.ts
+++ server/db.ts
@@ -1,13 +1,14 @@
-import { Pool, neonConfig } from '@neondatabase/serverless';
+// import { Pool, neonConfig } from '@neondatabase/serverless';
 // import { drizzle } from 'drizzle-orm/neon-serverless';
+import { Pool } from 'pg'
 import { drizzle } from 'drizzle-orm/node-postgres';
-import ws from "ws";
+// import ws from "ws";
 import * as schema from "@shared/schema";
 import dotEnv from "dotenv"
 import fs from 'fs'
 
 dotEnv.config()
-neonConfig.webSocketConstructor = ws;
+//neonConfig.webSocketConstructor = ws;
 
 if (!process.env.DATABASE_URL) {
   throw new Error(
