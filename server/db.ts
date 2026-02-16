f7687d3ae673088adce941dad78e6c480db2bedf
Phoeun-Chhantana <42382202+Phoeun-Chhantana@users.noreply.github.com>
2026-02-16 14:42:02 +0700

    update db ssl

--- server/db.ts
+++ server/db.ts
@@ -16,12 +16,12 @@ if (!process.env.DATABASE_URL) {
   );
 }
 
-//const certPath = process.env.NODE_EXTRA_CA_CERTS ?? ""
-const cert = Buffer.from(process.env.NODE_EXTRA_CA_CERTS ?? "", "base64")
+const certPath = process.env.NODE_EXTRA_CA_CERTS ?? ""
+//const cert = Buffer.from(process.env.NODE_EXTRA_CA_CERTS ?? "", "base64")
 
 export const pool = new Pool({ connectionString: process.env.DATABASE_URL, ssl: process.env.NODE_ENV === "production" && process.env.NODE_EXTRA_CA_CERTS ? {
   rejectUnauthorized: false,
-  //cert: fs.readFileSync(certPath).toString()
-  cert: cert
+  cert: fs.readFileSync(certPath).toString()
+  //cert: cert
 } : true });
 export const db = drizzle({ client: pool, schema });
